606515cecf5367663850632a10536bb7a82d5445
Jp1337 <jsk1921@gmail.com>
2014-05-02 03:35:05 -0400

    aeropuerto agregar habilitado + bdd modificada

--- controllers/aeropuertoController.js
+++ controllers/aeropuertoController.js
@@ -10,7 +10,7 @@ aeropuerto.prototype.get= function(req, res) {
 		database : 'aeropuerto'
 	});
 	connection.connect();
-	connection.query('SELECT * FROM airport', function(err, result) {
+	connection.query('SELECT CODE, NAME, COORDINATES, (SELECT NAME FROM city WHERE CITY_ID = city.CODE) FROM airport', function(err, result) {
  		res.render('aeropuertos', { data: result})
 	})
 	connection.end();
@@ -24,10 +24,10 @@ aeropuerto.prototype.crear = function(req, res) {
 		database : 'aeropuerto'
 	});
 	connection.connect();
-	// connection.query('SELECT id, nombre FROM airline', function(err, result) {
-	// 	console.log(result)
- 		res.render('aeropuertosCrear');
-	//})
+		connection.query('SELECT CODE, NAME FROM city', function(err, result) {
+		 	console.log(result)
+ 			res.render('aeropuertosCrear', { data: result });
+	})
 	connection.end();
 };
 
@@ -39,11 +39,11 @@ aeropuerto.prototype.insert=function(req,res) {
 		database : 'aeropuerto'
 		});
 	connection.connect();
-	connection.query('INSERT INTO airport SET ?',{	CODE: req.body.code , 
-													NAME: req.body.name,
-												 	COORDINATES: req.body.coordinates,
-												 	CITY_ID: req.body.city_id
-												 	},function(err, result, t){
+	connection.query('INSERT INTO airport SET ?',{	CODE: req.body.code, 
+							NAME: req.body.name,
+							COORDINATES: req.body.coordinates,
+							CITY_ID: req.body.city_id
+							},function(err, result, t){
   		if(err)
      		console.log('error');
 	    else{
